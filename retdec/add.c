d50bd9276df31e3d4e6fc11a8c287d1c3d30a4cc
Johannes Riecken <johannes.riecken@dunnhumby.com>
2023-11-05 12:34:25 +0100

    Add retdec

---
+++ retdec/add.c
@@ -0,0 +1,17 @@
+int add(int x, int y) {
+  return x + y;
+}
+
+int head(int arr[]) {
+  return arr[0];
+}
+
+int main() {
+  int arr[] = {1,2,3};
+  int arr2[] = {4,5,6,7};
+  int arr3[] = {8};
+  head(arr2);
+  head(arr3);
+  // return head(arr);
+  return 1;
+}
---
+++ retdec/setup.vim
@@ -0,0 +1,2 @@
+set keywordprg=perl\ -CA\ /home/jo/asm/retdec/keywordprg.pl
+set iskeyword=@,48-57,_,192-255,.
